710c721f63e908a5e6cf498bbfa45c9e3b2fef93
seymuur03 <seymuur03@gmail.com>
2024-10-18 20:38:04 +0400

    Task done refactor

--- Constants/obj/Release/net8.0/Constants.AssemblyInfo.cs
+++ Constants/obj/Release/net8.0/Constants.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Constants")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d892b9b57c15e3eb36a54087e2f9225032c93e8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Constants")]
